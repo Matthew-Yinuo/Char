4c1dbeda2bdb3ab280e3129bc41375526077a899
Matthew Hoth <matthewhoth@gmail.com>
2018-06-21 18:50:17 +0800

    Figure out how to flatten the data

--- char rect.py
+++ char rect.py
@@ -5,3 +5,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.utils import np_utils
+
+# fix random seed for reproducibility, 8 for the luckiness
+seed = 8
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
