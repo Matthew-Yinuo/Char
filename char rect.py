600bd0be3e71a1e1898a05a7251e25ec2dad606f
Matthew Hoth <matthewhoth@gmail.com>
2018-06-21 19:24:43 +0800

    Update the program by flattening the data and normalizing it

--- char rect.py
+++ char rect.py
@@ -11,3 +11,11 @@ seed = 8
 numpy.random.seed(seed)
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
